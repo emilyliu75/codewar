ea057e9aa215be49ac2993cc14061a891c63eab5
Xiangnan Liu <150020746+emilyliu75@users.noreply.github.com>
2025-04-12 21:37:03 +0100

    update class Fighter

--- kyu_7/declare_winner.py
+++ kyu_7/declare_winner.py
@@ -1,3 +1,12 @@
+class Fighter(object):
+    def __init__(self, name, health, damage_per_attack):
+        self.name = name
+        self.health = health
+        self.damage_per_attack = damage_per_attack
+
+    def __str__(self): return "Fighter({}, {}, {})".format(self.name, self.health, self.damage_per_attack)
+    __repr__=__str__
+
 def declare_winner(fighter1, fighter2, first_attacker):
     import math
     total_attack_to_win_for_1 = math.ceil(fighter1.health / fighter2.damage_per_attack)
@@ -12,3 +21,5 @@ def declare_winner(fighter1, fighter2, first_attacker):
             return fighter2.name
         else:
             return fighter1.name
+
+print(declare_winner(Fighter("Lew", 10, 2),Fighter("Harry", 5, 4), "Lew"))
