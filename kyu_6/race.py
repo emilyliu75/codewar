ec14bc626707d416720918d5cecbaff63d36ce10
Xiangnan Liu <150020746+emilyliu75@users.noreply.github.com>
2025-04-18 12:58:21 +0100

    cooking_time

--- kyu_6/race.py
+++ kyu_6/race.py
@@ -2,7 +2,7 @@ def race(v1, v2, g):
     if v2 <=v1:
         return None
     time_to_catch = g/(v2-v1) * 3600
-    # print(time_to_catch)
+
     hour = time_to_catch //3600
     minute = time_to_catch // 60 % 60
     second = time_to_catch % 60
